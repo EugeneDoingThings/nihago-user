092201421b935756bb911181a9eb62aa9231a0d7
Evgeniy Babadjanov <jeeugenebbj@gmail.com>
2022-04-21 19:44:09 +0300

    added seed for users table gitignore database

---
+++ scripts/user_seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.users (id, firstname, lastname, patronymic, date_of_birth, about, company_id, photo, created_at,
+                          updated_at)
+VALUES (DEFAULT, 'Eug', 'Bbj', 'Alex', '1996-02-12', 'Hey There', 1, 'example.com', '2022-04-01', '2022-04-01'),
+       (DEFAULT, 'Another', 'One', 'Bbj', '1996-02-12', 'Hey There Again', 2, 'example.too', '2022-04-04', '2022-04-04')
